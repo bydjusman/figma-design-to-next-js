3654f35a6cc9f993aa98cc65096ce477b67139aa
“[firstname <[bydjusman@gmail.com]>
2024-11-21 11:48:32 +0500

    your commit message

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className="py-6 px-4 bg-[#FAF5F5]">
+      <nav className="py-12 px-4 bg-[#FAF5F5]">
         <ul className="text-black font-semibold flex justify-end space-x-10 px-10">
           <li className="text-[18px] cursor-pointer">Work</li>
           <li className="text-[18px] cursor-pointer">Blog</li>
